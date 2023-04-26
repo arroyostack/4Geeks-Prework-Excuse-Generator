c89beedcd50fd36f75a9c12cea99838cd2f58d04
arroyostack <109061270+arroyostack@users.noreply.github.com>
2023-04-26 12:39:01 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const formExcuse = () => {
+    const displayExcuse = document.getElementById("excuse");
+
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    const phrase = [who, action, what, when];
+
+    let excuse = "";
+
+    phrase.forEach(item => {
+      excuse += item[Math.floor(Math.random() * item.length)] + " ";
+    });
+
+    displayExcuse.innerText = excuse;
+  };
+
+  formExcuse();
 };
